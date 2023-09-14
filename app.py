508a097368fff744794a633a2db7731289ba2181
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2023-09-13 22:19:27 -0300

    📦 NEW: 014

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from PIL import Image
+from streamlit_folium import st_folium
+import folium
 
 img = Image.open('./image/4744315.png')
 
@@ -15,3 +17,11 @@ st.set_page_config(
     })
 
 st.title('APP Em Construção')
+
+m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+folium.Marker(
+    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+).add_to(m)
+
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width=725)
