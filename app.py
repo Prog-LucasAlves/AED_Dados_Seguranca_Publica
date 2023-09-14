ac097fef6275a40b56575299a80b5e1bbdc82cca
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2023-09-13 21:49:58 -0300

    📦 NEW: 010

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 from PIL import Image
-import requests
 
-img = Image.open('https://cdn-icons-png.flaticon.com/24/1085/1085456.png')
-url = 'https://cdn-icons-png.flaticon.com/24/1085/1085456.png'
-response = requests.get(url)
+img = Image.open('./image/4744315.png')
 
-st.beta_set_page_config(page_title='Em Construção', page_icon=response)
+st.beta_set_page_config(page_title='Em Construção', page_icon=img)
