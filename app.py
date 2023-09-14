3c9c624d8ba2cae23325bf0347f5307775d5074e
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2023-09-13 23:13:32 -0300

    📦 NEW: 026

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ map = folium.Map(
     tiles='CartoDB positron')
 
 # call to render Folium map in Streamlit
-st_data = st_folium(map, width=200, height=450)
+st_data = st_folium(map, width=400, height=50)
