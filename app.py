bbdb4e7435d60db74851231a121a319df9e63c20
Lucas Alves <lucasalves_taz@hotmail.com>
2024-08-23 10:57:05 -0300

    up pre-commit-config.yaml

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 # Bibliotecas utilizadas
+# import folium.map
 import streamlit as st
 from PIL import Image
 import duckdb
+# import folium
+# from streamlit_folium import st_folium
 
 # Configurações da página
-st.set_option("deprecation.showfileUploaderEncoding", False)
 img = Image.open("./image/4744315.png")
 
 st.set_page_config(
@@ -19,25 +21,69 @@ st.set_page_config(
     },
 )
 
+# Caminho do arquivo parquet com os dados
+path_parquet = "./data/raw_data/GOLDEN/GOLDEN_data.parquet"
 
-# Leitura dos dados
-def loaddata():
-    con = duckdb.connect()
-    data = con.execute(
-        "SELECT * FROM read_parquet('./data/raw_data/GOLDEN/GOLDEN_data.parquet')"
-    ).df()
-    con.close()
-    return data
+# Caminho do arquivo com as descrições das variáveis
+path_descricoes = "./data/dict_data/tipo_ocorrencia.csv"
 
+# Título do dashboard
+title = """
+<p style="color:Black; font-size: 40px; font-weight: bolder;"
+> Dashboard dos Dados da Segurança Pública do Estado do RJ </p>
+"""
+st.markdown(title, unsafe_allow_html=True)
 
-# Visualização dos dados
-def showdata():
-    st.title("Dados da Segurança Pública do Estado do Rio de Janeiro")
-    # Carregando os dados
-    data = loaddata()
-    # Mostrando os dados
-    st.dataframe(data)
+# Colunas do dashboard
+col1, col2, col3 = st.columns(3)
 
+with col1:
+    # Carregando as datas(ano)
+    ano_df = duckdb.query(
+        f"""SELECT DISTINCT ano
+        FROM '{path_parquet}'
+        ORDER BY ano"""
+    ).to_df()
+    ano = st.selectbox("Selecione o Ano", ano_df)
 
-if __name__ == "__main__":
-    showdata()
+with col2:
+    # Carregando o arquivo das descrições das variáveis
+    titulo_df = duckdb.query(
+        f"""SELECT descricao
+        FROM '{path_descricoes}'"""
+    ).to_df()
+    titulo = st.selectbox("Titulo", titulo_df)
+
+with col3:
+    # Criando informação com o total de ocorrencias(Geral)
+    titulo_ocorrencia = duckdb.query(
+        f"""SELECT tipo_ocorrencia
+        FROM '{path_descricoes}'
+        WHERE descricao = '{titulo}'"""
+    ).to_df()
+    titulo_ocorrencia = titulo_ocorrencia.iloc[0, 0]
+    st.markdown("TOTAL DE OCORRÊNCIAS")
+
+    total_titulo_df = duckdb.query(
+        f"""SELECT SUM({titulo_ocorrencia})
+        FROM '{path_parquet}'"""
+    ).to_df()
+    total_titulo_df = total_titulo_df.iloc[0, 0]
+    total_titulo = st.markdown(f"{total_titulo_df:.0f}")
+
+# Criando Mapa
+path_mapa = "./data/map/geojs-33-mun.json"
+
+# m = folium.Map(location=([-22.42, -42.48]), zoom_start=7)
+
+# style = lambda x: {"color": "#000000", "fillOpacity": 0, "weight": 1}
+
+# folium.GeoJson(path_mapa, style_function=style).add_to(m)
+
+# st_folium(m, height=350, width=790)
+
+"""
+folium.Choropleth(
+    geo_data=d).add_to(m)
+st_map = st_folium(m, height=350, width=750)
+"""
