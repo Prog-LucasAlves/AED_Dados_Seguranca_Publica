085204677a7acc51a71588e7d918be46f1579ee9
Lucas Alves <lucasalves_taz@hotmail.com>
2024-08-30 12:28:28 -0300

    up extract_name_columns.py

--- SRC/extract/extract_name_columns.py
+++ SRC/extract/extract_name_columns.py
@@ -2,7 +2,14 @@
 import pandas as pd
 
 # Criando dicionário com a colunas
-tipo_ocorrencia = {"hom_doloso": "Homicídio doloso"}
+tipo_ocorrencia = {
+    "hom_doloso": "Homicídio doloso",
+    "lesao_corp_morte": "Lesão corporal seguida de morte",
+    "latrocinio": "Latrocínio (roubo seguido de morte)",
+    "cvli": "Crimes Violentos Letais Intencionais*",
+    "hom_por_interv_policial": "Morte por intervenção de agente do Estado",
+    "letalidade_violenta": "Letalidade violenta*",
+}
 
 # Transformando em um dataframe e resetando o index
 tipo_ocorrencia = pd.DataFrame.from_dict(tipo_ocorrencia, orient="index").reset_index()
